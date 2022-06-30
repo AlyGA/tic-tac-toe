25447eb3d60466cbfd880f67d5e65955076c62f0
Aly Anderson <aly779372@gmail.com>
2022-06-30 16:51:24 -0700

    Add module function pattern

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const boardPieces = document.querySelectorAll(".boardItem");
-
-boardPieces.forEach((item) => {
-  item.addEventListener("click", () => {
-    // If a box is already filled, do nothing.
-    if (item.textContent !== "") {
-      alert("This box is already filled! Pick another box");
-      return;
-    }
-
-    // Checking to see which option a user picked.
-    const choice = prompt("Which would would you pick? Give either an X or O");
-    if (choice === "O" || choice === "o") {
-      item.textContent = "O";
-    } else if (choice === "X" || choice === "x") {
-      item.textContent = "X";
-    } else {
-      alert("Invalid input! Make sure it's an X or O!");
-    }
-  });
-});
-
 // Gameboard object
-const gameBoard = (score) => {
-  return { score };
+const gameBoard = () => {
+  const gameArray = [];
+  const boardPieces = document.querySelectorAll(".boardItem");
+
+  // Event listener to allow user to choose their X or O option.
+  let options = boardPieces.forEach((item) => {
+    item.addEventListener("click", () => {
+      // If a box is already filled, do nothing.
+      if (item.textContent !== "") {
+        alert("This box is already filled! Pick another box");
+        return;
+      }
+
+      // Checking to see which option a user picked.
+      const choice = prompt(
+        "Which would would you pick? Give either an X or O"
+      );
+      if (choice === "O" || choice === "o") {
+        item.textContent = "O";
+        gameArray.push(item.textContent);
+      } else if (choice === "X" || choice === "x") {
+        item.textContent = "X";
+        gameArray.push(item.textContent);
+      } else {
+        alert("Invalid input! Make sure it's an X or O!");
+      }
+    });
+  });
+  return { gameArray, options };
 };
+gameBoard();
